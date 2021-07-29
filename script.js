a32034c86ce2c88b5dcb637057e478d01d788bf2
gigiftttgi <natcha_si@cmu.ac.th>
2021-07-29 14:05:36 +0700

    1

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
+const btn_reset = document.querySelector("#reset");
 
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  // your code here
+  if(btn_toggle.innerText === "Show Calculation"){
+    btn_toggle.innerText = "Show Author";
+    author.innerText = "630610727"
+}else{
+    btn_toggle.innerText= "Show Calculation";
+    author.innerText = "630610727 Natcha Silakorn";
+}
 }
 
 // more codes for Search and Reset buttons here
