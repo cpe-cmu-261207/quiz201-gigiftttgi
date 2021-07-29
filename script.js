b5f60628a9f22eb761e71252a1c384212925b4d1
gigiftttgi <natcha_si@cmu.ac.th>
2021-07-29 14:19:34 +0700

    1

--- script.js
+++ script.js
@@ -4,17 +4,28 @@ const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
 const btn_reset = document.querySelector("#reset");
+const btn_search = document.querySelector("#search");
 
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
   if(btn_toggle.innerText === "Show Calculation"){
     btn_toggle.innerText = "Show Author";
-    author.innerText = "630610727"
+    author.innerText = "630610727length.value";
 }else{
     btn_toggle.innerText= "Show Calculation";
     author.innerText = "630610727 Natcha Silakorn";
 }
 }
+
+btn_search.onclick = () => {
+  var characters = 0;
+  for (var i = 0, length = string.length(); i < length; i++) {
+  if (str.charAt(i) != ' ') {
+    characters++;
+  }
+}
+}
+
 
 // more codes for Search and Reset buttons here
